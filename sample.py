188e6afaea88f19cde95669d507f4fdb3bb76a09
Norman Heckscher <norman.heckscher@gmail.com>
2017-02-03 16:21:42 +1000

    update help
    better reflect loading rather than storing when sampling.

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ from model import Model
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save',
-                       help='model directory to store checkpointed models')
+                       help='model directory to load stored checkpointed models from')
     parser.add_argument('-n', type=int, default=200,
                        help='number of words to sample')
     parser.add_argument('--prime', type=str, default=' ',
